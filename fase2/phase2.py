d8b82809ce9e189d9857896808f7deecd6b82190
DominoWw <dominikwolowczyk@wp.pl>
2023-05-05 14:22:14 +0200

    find ready

--- fase2/phase2.py
+++ fase2/phase2.py
@@ -10,81 +10,76 @@ from bst import BinarySearchTree
 
 class BST2(BinarySearchTree):
     def find_dist_k(self, n: int, k:int) -> list:
-        if self._root:
-
-            #checking whether exists simultaneously checking the depth
-            N=self.search(n)
-            if N:
-                
-                d=self.depth(N)
-                L=[]
-                self._parent_nodes(n, k, d, self._root, L, N)
-                return L
-
-
-
-            
+        #lets check whether the node exist
+        N=self.search(n)
+        if N:   
+            L=[]
+            #if the distance is 0, we are interested just in that particular node, so the placement does not matter
+            if k==0:
+                L.append(n)
+            #if k is greater than 0, we have evaluate the whole tree
             else:
-                return []
-            
+                #d is going to be used as depth of the searched node
+                d=self.depth(N)
+                #as the function will be comparing the values, distance, we provide that parameters,
+                #apart from that, it will also check de depth difference - hence d
+                #the funcion _parent_nodes is going to be used recursively on nodes - hence we provide a node (here root)
+                #proper nodes' values are going to be placed in the list L, that is going to be returned at the end
+                #for convenience, in order not to check the node with .search() function, we provide node N as well 
+                self._parent_nodes(n, k, d, self._root, L, N)
+            return L
         else:
             return []
 
 
-
+        #this function evaluates the subtrees starting with root and then proceeding in the direction of the N node
+        #once it reaches N, calls a function listing elements for the last time and does not proceed further into the tree
     def _parent_nodes(self, n:int, k:int, d:int, node:BinaryNode, L:list, N:BinaryNode):
         if node==None:
             return
-        elif node==N:
-            
-            self._list_elem(k-1, node.left, L)
-            self._list_elem(k-1, node.right, L)
 
-        
-        
-        #node is on the left-hand side of the tree
+        #node is on the left-hand side of the subtree
         elif node.elem > n:
-
+            #at first lets check whether this is the node we should list
             if k==d-self.depth(node):
                 L.append(node.elem)
-            
+            #if not - we search for elements to be listed on the right-hand side of the subtree
             else:
-                self._list_elem(k- 1- (d-self.depth(node)), node.right, L)
-            
+                            #we decrease de declared distance in regard to the difference of depths od the nodes
+                self._list_elem(k- (d-self.depth(node)), node.right, L)
+            #either way, we proceed towards the wanted node    
             self._parent_nodes(n, k, d, node.left, L, N)
 
-        
+        #node is on the right hand side of the subtree
         elif node.elem < n:
-
             if k==d-self.depth(node):
                L.append(node.elem)
-            
             else:
-                self._list_elem(k- 1- (d-self.depth(node)), node.left, L)
-            
+                self._list_elem(k- (d-self.depth(node)), node.left, L)
             self._parent_nodes(n, k, d, node.right, L, N)
-            
-        
-        
 
-             
-
-
-
-
-
-    #listing elements from the other side of the parent node
-    #if x == 0 -> thats the element we are looking for, if grater than 0 -> we have to search children
+        #once we found the node, the only thing that is left is to find elements in the subtree 
+        elif node==N:
+            self._list_elem(k, node.left, L)
+            self._list_elem(k, node.right, L)
+ 
+    
+    #listing elements that are in a given distance from parent node
+    #if x == 1 -> thats the element we are looking for, if grater than 1 -> we have to search children
     def _list_elem(self, x:int, node:BinaryNode, L:list):
         if node==None:
             return
-        elif x==0:
+        #if the distance is 1 - this is the element we are looking for
+        # (we were searching for a node that is placed 1 -unit- from the origin node)
+        elif x==1:
             L.append(node.elem)
             return
-        elif x>0:
+        elif x>1:
+            #if the distance is greater than 1, we decrease it and proceed to search
             self._list_elem(x-1, node.left, L)
             self._list_elem(x-1, node.right, L)
-
+        else:
+            return
         
 
 
@@ -104,27 +99,35 @@ if __name__ == '__main__':
     # input_list_01 = [5, 1, 7, 9, 23]
     # input_list_02 = [1, 9, 11]
 
-    tree0=BST2()
-    for x in input_list:
-        tree0.insert(x)
-    tree0.draw()
+    task1 = [14,11,18,19,16,13,10,5,12,15,17,30,31,29,6,4,2,8,24,33,34,32,25,23,9,7,3,1,21,27,36,37,35,28,26,22,20]
+
+    treeTask1=BST2()
+    for x in task1:
+        treeTask1.insert(x)
+    treeTask1.draw()
+
+
+    # tree0=BST2()
+    # for x in input_list:
+    #     tree0.insert(x)
+    # tree0.draw()
 
 
 
 
-    input_list_01 = [5, 12, 2, 1, 3, 9]
-    input_list_02 = [9, 3, 21]
+    # input_list_01 = [5, 12, 2, 1, 3, 9]
+    # input_list_02 = [9, 3, 21]
 
     # Build and draw first tree
-    tree1 = BST2()
-    for x in input_list_01:
-        tree1.insert(x)
+    # tree1 = BST2()
+    # for x in input_list_01:
+    #     tree1.insert(x)
     #tree1.draw()
 
     # Build and draw second tree
-    tree2 = BinarySearchTree()
-    for x in input_list_02:
-        tree2.insert(x)
+    # tree2 = BinarySearchTree()
+    # for x in input_list_02:
+    #     tree2.insert(x)
     #tree2.draw()
 
     function_names = ["merge", "intersection", "difference"]
@@ -138,5 +141,5 @@ if __name__ == '__main__':
     '''
 
 
-    X=tree0.find_dist_k(15,1)
+    X=treeTask1.find_dist_k(30,5)
     print(X)
